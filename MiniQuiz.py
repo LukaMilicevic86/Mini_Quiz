a295f74e086778ba9b8c0b13d619aa92ae1dbdea
Luka Milicevic <lmilicevicmtk@gmail.com>
2026-03-26 19:48:15 +0100

    Add the .py file, create the first screen class, define checkbox function for it

---
+++ MiniQuiz.py
@@ -0,0 +1,21 @@
+
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+Builder.load_file("MiniQuiz.kv")
+
+class FirstScreen(Widget):
+    answers = []
+    correct_answers = ["Thor", "Hulk"]
+
+    def chckbox_click1(self, instance, value, selected):
+        if value == True:
+            self.answers.append(selected)
+            self.ids.display1.text = f"Your selection: {self.answers}"
+        else:
+            self.answers.remove(selected)
+            self.ids.display1.text = f"Your selection: {self.answers}"
+            
+        
