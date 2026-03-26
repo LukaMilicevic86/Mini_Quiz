84e0eb0c9f6bd12a6007dcf0ed5e5d54b589f07a
Luka Milicevic <lmilicevicmtk@gmail.com>
2026-03-26 19:55:01 +0100

    Add the second screen class

--- MiniQuiz.py
+++ MiniQuiz.py
@@ -18,4 +18,17 @@ class FirstScreen(Widget):
             self.answers.remove(selected)
             self.ids.display1.text = f"Your selection: {self.answers}"
             
-        
+
+
+class SecondScreen(Widget):
+    answers = []
+    correct_answers = ["Superman", "Batman"]
+
+    def chckbox_click2(self, instance, value, selected):
+        if value == True:
+            self.answers.append(selected)
+            self.ids.display2.text = f"Your selection: {self.answers}"
+        else:
+            self.answers.remove(selected)
+            self.ids.display2.text = f"Your selection: {self.answers}"
+    
